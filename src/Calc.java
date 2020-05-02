fd45272c2c1509a9959cb35e543291074f673dbb
Markus Hopf <markus@hopf-it.de>
2020-05-02 16:03:54 +0200

    Add files via upload

---
+++ src/Calc.java
@@ -0,0 +1,21 @@
+
+public class Calc {
+
+	public static void main(String[] args) {
+		System.out.println("Start!!!");
+
+		Point p1 = new Point(1, 4);
+		Point p2 = new Point(4, 4);
+		Point p3 = new Point(3, 2);
+		Point p4 = null; //new Point(-5, );
+		
+		Point[] points = {p1, p2, p3, p4};
+
+		double minDistance = Point.determineMinimum(p1, p2);
+		System.out.println("Distance:"+ minDistance);
+		
+		double minDistanceOfArray = Point.determineMinimumOfArray(points);
+		System.out.println("Distance of Array:"+ minDistanceOfArray);
+		
+	}
+}
