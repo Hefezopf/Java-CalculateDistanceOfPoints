d3b2923db4736cb559220247bff47680cb5c22ab
Hefezopf <markus@hopf-it.de>
2020-05-03 14:58:12 +0200

    mvn

---
+++ src/main/java/hopf/de/App.java
@@ -0,0 +1,23 @@
+package hopf.de;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+		System.out.println("Start!!!");
+
+		Point p1 = new Point(1, 4);
+		Point p2 = new Point(2, 3);
+		Point p3 = new Point(3, 2);
+		Point p4 = new Point(4, 1);
+		
+		Point[] points = {p1, p2, p3, p4};
+
+		double minDistance = Point.determineMinimum(p1, p2);
+		System.out.println("Distance:"+ minDistance);
+		
+		double minDistanceOfArray = Point.determineMinimumOfArray(points);
+		System.out.println("Distance of Array:"+ minDistanceOfArray);
+
+    }
+}
